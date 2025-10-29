4fa9be3baf3b61f825d887d6b1b4d903c1df9388
worldofhope <n.sadova98@gmail.com>
2025-10-29 13:21:48 +0500

    update project

--- src/components/Card/HistoryCard/HistoryCard.tsx
+++ src/components/Card/HistoryCard/HistoryCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import Button from '@/components/Button';
 import { StarIcon } from '../../Icons';
 
 import s from './HistoryCard.module.scss';
@@ -19,7 +20,11 @@ export default function HistoryCard({img, id, title, price, date, status} :Histo
     <div className={s.card}>
       <div className={s.card__img}><img src={img} alt="" /></div>
       <div className={s.card__box}>
-        <div className={s.card__date}>Дата: {date}</div>
+        <div className={s.card__top}>
+          <div className={s.card__date}>{date}</div>
+          {/* <div className={classNames(s.card__status, 'ready')}>выполнен</div> */}
+          <div className={classNames(s.card__status)}>в обработке</div>
+        </div>
         <div className={s.card__info}>
           <div className={s.card__num}>Заказ №{id}</div>
           <div className={s.card__title}>{title}</div>
@@ -29,6 +34,11 @@ export default function HistoryCard({img, id, title, price, date, status} :Histo
           </div>
         </div>
       </div>
+      <Button
+          className={classNames(s.card__button, 'button button-orange')}
+        >
+          Отменить заказ
+        </Button>
     </div>
   )
 }
